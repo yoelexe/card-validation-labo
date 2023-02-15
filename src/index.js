df9a48cf8d9fcd494014887f10a4ee72ad1e888c
yoelexe <karmahucents@gmail.com>
2023-02-14 20:31:41 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import validator from './validator.js';
+import {isValid, cardVerificationNum} from './validator.js';
 
-console.log(validator);
+/* Busca a partir de la carpeta doinde esto -> ./ */
+
+
+//const form = document.querySelector("#credit-card");
+
+
+const cardNumber = document.querySelector("#cardno");
+const button = document.querySelector("#validator");
+// imagen de tarjeta
+//const cardNumberText = document.querySelector(".number-vl");
+
+// keyup o input
+cardNumber.addEventListener("keyup", cardVerificationNum)
+
+button.addEventListener("click", isValid)
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,88 @@
-const validator = {
-  // ...
-};
 
-export default validator;
+/* PRUEBA con función de flecha
+const validator = (a,b) => {
+    console.log("Hola ")
+    return a+b
+};
+*/
+
+export const cardVerificationNum = () => {
+  const cardNumber = document.querySelector("#cardno");
+  // imagen de tarjeta
+  //const cardNumberText = document.querySelector(".number-vl");
+
+  cardNumber.addEventListener("keyup", (e) => {
+    if(!e.target.value){
+      //cardNumberText.innerHTML = "1234 5678 9101 1121";
+      //console.log("Funciona")
+    }else{
+      const valuesOfInput = e.target.value.replaceAll(" ", "");        
+    
+      if(e.target.value.length > 14){
+        e.target.value = valuesOfInput.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
+      // ^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})$
+      //cardNumberText.innerHTML = valuesOfInput.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
+      }else if(e.target.value.length > 9){
+        e.target.value = valuesOfInput.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3");
+      }else if(e.target.value.length > 4){
+        e.target.value = valuesOfInput.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+      }
+    }
+  })
+}
+
+const algorithm = (cardno) => {
+  
+  const cardReverse = cardno.split(' ').join('').split('').reverse()
+
+  let count = 0;
+  let reverseNum = cardReverse;
+  
+  let pairNum;
+
+  for (let i = 0; i < reverseNum.length; i++){
+
+    if((i+1) % 2 === 0){
+      pairNum = (reverseNum[i] * 2);
+      if(pairNum >= 10){
+        const firstDigit = ((pairNum - 10) + 1);
+        reverseNum[i] = firstDigit;
+      }else{
+        reverseNum = pairNum;
+      }
+    }
+    
+    count += reverseNum[i];
+  }
+
+  // eslint-disable-next-line eqeqeq
+  if(count % 10 === 0){
+    console.log('Tarjeta válida')
+    return true;
+  }else{
+    console.log('Tarjeta inválida')
+    return false;
+  }
+
+}
+
+export const isValid = () => {
+  const allNumber = document.getElementById('cardno');
+  //const reverse = allNumber.split('').reverse().join('');
+  //const allValidator = document.getElementById('ccValidator');
+  //let message = "";
+
+  
+  
+  if(algorithm(allNumber.value)){
+    //message = "that worked!";
+    console.info("that worked")
+          
+  }else{
+    //message = "verification failed :(";
+    console.warn("verification failed")
+  }
+}
+
+
+//export default validator;
